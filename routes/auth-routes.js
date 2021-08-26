4dbc8f35c9e47c2815cd47fb75e54cec46312e52
samox_ <samirasteinmayer@web.de>
2021-08-26 21:40:38 +0200

    signup route fixed

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -31,8 +31,10 @@ router.post("/signup/buddy", (req, res, next) => {
         password1 = password[0]
     }
     else {
-        res.send('Please confirm your password again.')
+        res.render('users/signupBuddy', { errorMessage: 'Please confirm your password again.' })
+        return;
     }
+
     let choiceOfAction =[]
 
     //task: Storing in choiceOfAction which checkboxes are activated: dailyTasks/hangignOut/teaching
@@ -48,8 +50,8 @@ router.post("/signup/buddy", (req, res, next) => {
 
     // all fields have to be filled stays untouched
     //   if (!username || !email || !password || !birthday || !choiceOfAction) {
-    if (!username || !email || !password1 || !birthday) {
-        console.log("not all fields ...")
+    if (!username || !email || !password || !birthday) {
+        console.log("not all mandatory input is given..")
         res.render('users/signupBuddy', { errorMessage: 'All fields are mandatory. Please provide all required input.' })
         return;
     }
@@ -61,6 +63,7 @@ router.post("/signup/buddy", (req, res, next) => {
             .render('users/signupBuddy', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
         return;
     }
+
     //creating the according data in db, but using the encrypted version:
     //generate salt
     const salt = bcrypt.genSaltSync(saltRounds);
@@ -77,20 +80,20 @@ router.post("/signup/buddy", (req, res, next) => {
             console.log('A new buddy has joined the pool: ', userFromDB);
             res.redirect('/auth/buddyView');
         })
-    //   .catch(error => {
-    //       if (error instanceof mongoose.Error.ValidationError) {
-    //           res.status(500).render('users/signupBuddy', {
-    //               errorMessage: error.message
-    //           });
-    //       } else if (error.code === 11000) {
-    //           res.status(500).render('users/signupBuddy', {
-    //               errorMessage: 'Username and email need to be unique. Either username or email is already used.'
-    //           });
-    //       }
-    //       else {
-    //           next(error);
-    //       }
-    //   });
+      .catch(error => {
+          if (error instanceof mongoose.Error.ValidationError) {
+              res.status(500).render('users/signupBuddy', {
+                  errorMessage: error.message
+              });
+          } else if (error.code === 11000) {
+              res.status(500).render('users/signupBuddy', {
+                  errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+              });
+          }
+          else {
+              next(error);
+          }
+      });
 })
 
 
@@ -128,22 +131,45 @@ router.post("/signup/tiger", (req, res, next) => {
     console.log("User input (tiger):", req.body);
     //storing the userinput 
 
-    //---------> OPEN:
-    //defining array content choiceOfAction, taken from checkboxes
-
-
+    
+//////
+//storing the userinput 
     //usertype still undefined and needs to be preset for this formvalidation 
-    const { username, email, password, birthday, choiceOfAction } = req.body;
+    const { username, email, password, birthday, hangingOut, dailyTasks, teaching } = req.body;
+
+    //reducing the passwords to one
+    //still open: check if both inputs are the same
+    let password1 
+    if (password[0] === password[1]) {
+        password1 = password[0]
+    }
+    else {
+        res.render('users/signupBuddy', { errorMessage: 'Please confirm your password again.' })
+        return;
+    }
+
+    let choiceOfAction =[]
+    //task: Storing in choiceOfAction which checkboxes are activated: dailyTasks/hangignOut/teaching
+    if(dailyTasks === "on") {
+        choiceOfAction.push('dailyTasks')
+    } 
+    if(hangingOut == "on" ) {
+        choiceOfAction.push('hangingOut')
+    }
+    if(teaching == "on" ) {
+        choiceOfAction.push('teaching')
+    } 
 
     // all fields have to be filled stays untouched
-    if (!username || !email || !password || !birthday || !choiceOfAction) {
+    //   if (!username || !email || !password || !birthday || !choiceOfAction) {
+    if (!username || !email || !password || !birthday) {
         res.render('users/signupTiger', { errorMessage: 'All fields are mandatory. Please provide all required input.' })
         return;
     }
 
     //make sure passwords are strong
     const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-    if (!regex.test(password)) {
+    if (!regex.test(password1)) {
         res.status(500)
             .render('users/signupTiger', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
         return;
@@ -152,9 +178,9 @@ router.post("/signup/tiger", (req, res, next) => {
     //generate salt
     const salt = bcrypt.genSaltSync(saltRounds);
     //create a hashed version of the password:
-    const hash1 = bcrypt.hashSync(password, salt);
+    const hash1 = bcrypt.hashSync(password1, salt);
 
-    User.create({ username: username, email: email, password: hash1, usertype: "tiger", bithday: birthday, choiceOfAction: choiceOfAction })
+    User.create({ username: username, email: email, passwordHash: hash1, usertype: "inNeed", bithday: birthday, choiceOfAction: choiceOfAction })
         // .then(() => {
         //     res.send("user created")
         // })
@@ -230,7 +256,7 @@ router.post('/login', (req, res, next) => {
             if (!user) {
                 res.render('users/login', { errorMessage: 'Email is not registered. Try with other email.' });
                 return;
-            } else if (bcryptjs.compareSync(password, user.password)) {
+            } else if (bcrypt.compareSync(password, user.password)) {
                  // when we introduce session, the following line gets replaced with what follows:
                  // res.render('users/user-profile', { user });
 

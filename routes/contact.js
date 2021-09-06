ae3df1c0ad5c16e92f6320e56e70c486f11c2207
samox_ <samirasteinmayer@web.de>
2021-09-06 14:30:01 +0200

    tidy the routes of tiger and buddy, some basic styling

--- routes/contact.js
+++ routes/contact.js
@@ -12,91 +12,118 @@ const Message = require("../models/Message.model");
 //if buddy clicks on tiger preview (in auth/buddyView)  he gets redirected here to show the whole selfdescription of the tiger.
 //here he can click on a button to message this tiger or go back to collection.
 
-router.get('/tigerslist/:id', (req, res, next) => {
-  const { id } = req.params;
-  User.findById(id)
-    .then(tigerDetails => {
-      res.render('interaction/showTigerDetails', tigerDetails)
+router.get('/:id', (req, res, next) => {
+  //access user in session:
+  // req.session.currentUser = user;
+  User.findById(req.session.currentUser._id)
+    .then((user) => {
+      //safe choosen tiger_id
+      const { id } = req.params;
+      User.findById(id)
+        .then(tigerDetails => {
+          res.render('interaction/showTigerDetails', { tigerDetails: tigerDetails, userInSession: user })
+        })
     })
   // .catch(error => console.log(`Error while updating the celeb details: ${error}`))
 })
 
-//message to tiger
-router.get('/tigerslist/:id/message', (req, res, next) => {
-  const { id } = req.params;
-  User.findById(id)
-    .then(tigerContact => {
-      res.render('interaction/mess2tiger', tigerContact)
-    })
+//messaging
+router.get('/:id/message', (req, res, next) => {
+  //access/safe user in session:
+  User.findById(req.session.currentUser._id)
+    .then((user) => {
+      //safe choosen tiger_id
+      const { id } = req.params;
+      User.findById(id)
+        .then(tigerContact => {
+          if(user.usertype=='buddy'){
+            res.render('interaction/mess2tiger', { tigerContact: tigerContact, userInSession: user })
+          }
+          else{
+            res.render('interaction/mess2buddy', { tigerContact: tigerContact, userInSession: user })
+          }
+        })
+    });
   // .catch(error => console.log(`Couldnt reach tiger contact: ${error}`))
 })
 
-router.post('/tigerslist/:id/message', (req, res, next) => {
-  const message = req.body.typedMessage;
-  const userID_messageOriginBuddy = req.params.id
-  Message.create({ receiver: userID_messageOriginBuddy, sender: req.session.currentUser._id, content: message })
-    .then(() => {
-      res.redirect('/interact/inbox')
-    })
+router.post('/:id/message', (req, res, next) => {
+  //access/safe user in session:
+  User.findById(req.session.currentUser._id)
+    .then((user) => {
+      const message = req.body.typedMessage;
+      const userID_receiver = req.params.id
+      Message.create({ receiver: userID_receiver, sender: req.session.currentUser._id, content: message })
+        .then(() => {
+          res.redirect('/interact/inbox', { userInSession: user })
+        })
+    });
 })
 
 
-/////Inbox
+/////I N B O X //////////
 
-//inbox buddy
 router.get('/inbox', (req, res) => {
-  
   User.findById(req.session.currentUser._id)
     .then((user) => {
       Message.find({ receiver: user }).populate('sender')
         .then((myMessages) => {
-          console.log("mssg",myMessages);
-          res.render('interaction/inboxAll', { msgs: myMessages, userInSession:user })
-
+          console.log("msgs", myMessages);
+          if(user.usertype=="buddy"){
+            res.render('interaction/inboxBuddy', { msgs: myMessages, userInSession: user })
+          }
+          else{
+            res.render('interaction/inboxTiger', { msgs: myMessages, userInSession: user })
+          } 
         })
-    })
+    });
 })
 
-router.post('/inbox', (req, res) => {
-  const message = req.body.typedMessage;
-  const userID_messageOriginBuddy = req.params.id
-  Message.create({ receiver: userID_messageOriginBuddy, sender: req.session.currentUser._id, content: message })
-    .then(() => {
-      res.redirect('/interact/inbox')
-    })
-})
+//no option of writing messages in the inbox. Just in the /message route.
+//
+// router.post('/inbox', (req, res) => {
+//   const message = req.body.typedMessage;
+//   const userID_messageOriginBuddy = req.params.id
+//   Message.create({ receiver: userID_messageOriginBuddy, sender: req.session.currentUser._id, content: message })
+//     .then(() => {
+//       res.redirect('/interact/inbox')
+//     })
+// })
 
 
 ////T I G E R ___ V I E W //////
+
+//not necessary:
+
 //message to buddy
 //on tigerView the tiger sees the inbox
-//By clicking on one message he goes here:
-router.get('/message2buddy/:id', (req, res) => {
-  if (!req.session.currentUser) {
-    res.send('We are sorry, you re just able to see this if you are logged in')
-    res.redirect('/auth/login');
-  }
-  else {
-    User.findById(req.session.currentUser._id)
-      .then((user) => {
-        res.render('/interact/mess2tiger/:id', {
-          userInSession: user
-        });
+// //By clicking on one message he goes here:
+// router.get('/:id/message', (req, res) => {
+//   if (!req.session.currentUser) {
+//     res.send('We are sorry, you re just able to see this if you are logged in')
+//     res.redirect('/auth/login');
+//   }
+//   else {
+//     User.findById(req.session.currentUser._id)
+//       .then((user) => {
+//         res.render('/interact/:id', {
+//           userInSession: user
+//         });
 
-      });
-  }
-})
+//       });
+//   }
+// })
 
-router.post('/message2buddy/:id', (req, res) => {
+// router.post('/:id/message', (req, res) => {
 
-  //accessing the user that has send the message the tiger wants to reply to for linking the message in the DB
-  const message = req.body.typedMessage;
-  const userID_messageOriginTiger = req.params.id
-  Message.create({ receiver: userID_messageOriginTiger, sender: req.session.currentUser._id, content: message })
-    .then(() => {
-      res.send('functions')
-    });
-})
+//   //accessing the user that has send the message the tiger wants to reply to for linking the message in the DB
+//   const message = req.body.typedMessage;
+//   const userID_messageOriginTiger = req.params.id
+//   Message.create({ receiver: userID_messageOriginTiger, sender: req.session.currentUser._id, content: message })
+//     .then(() => {
+//       res.send('works')
+//     });
+// })
 
 
 
